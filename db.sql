2b9c9747bcfc97b3019c34b74efab197c40813e3
nurhidayatullah <nur_hidayat_45@yahoo.com>
2015-05-19 19:42:19 +0700

    bfbvbvvbv

--- db.sql
+++ db.sql
@@ -25,11 +25,11 @@ CREATE TABLE `group_user` (
   `create_at` datetime DEFAULT NULL,
   `update_at` datetime DEFAULT NULL,
   PRIMARY KEY (`kode_group`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `group_user` */
 
-insert  into `group_user`(`kode_group`,`nama_group`,`create_at`,`update_at`) values (1,'Administrator','2015-04-25 19:05:00','2015-05-03 17:11:03'),(2,'Developer','2015-04-25 19:05:00','2015-04-26 05:30:10'),(8,'User','2015-04-25 17:53:23','2015-04-25 17:14:38');
+insert  into `group_user`(`kode_group`,`nama_group`,`create_at`,`update_at`) values (1,'Administrator','2015-04-25 19:05:00','2015-05-03 17:11:03'),(2,'Admin','2015-05-14 17:43:43',NULL);
 
 /*Table structure for table `menu` */
 
@@ -68,11 +68,11 @@ CREATE TABLE `role` (
   KEY `FK_role_menu` (`kode_menu`),
   CONSTRAINT `FK_role_group` FOREIGN KEY (`kode_group`) REFERENCES `group_user` (`kode_group`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_role_menu` FOREIGN KEY (`kode_menu`) REFERENCES `menu` (`kode_menu`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 
 /*Data for the table `role` */
 
-insert  into `role`(`kode_role`,`kode_group`,`kode_menu`,`view`,`itambah`,`iupdate`,`idelete`,`create_at`,`update_at`) values (1,1,17,1,1,1,1,'2015-05-06 02:37:41','2015-05-07 17:03:45'),(2,2,17,1,1,1,1,'2015-05-06 02:37:41','2015-05-07 17:56:44'),(3,8,17,0,0,0,0,'2015-05-06 02:37:41','2015-05-07 17:39:44'),(5,1,18,1,1,1,1,'2015-05-06 02:06:42','2015-05-08 15:32:49'),(6,2,18,1,1,1,1,'2015-05-06 02:06:42','2015-05-07 17:16:45'),(7,8,18,0,0,0,0,'2015-05-06 02:06:42','2015-05-07 17:08:45'),(9,1,19,1,1,1,1,'2015-05-06 02:41:42','2015-05-07 06:05:55'),(10,2,19,1,0,0,0,'2015-05-06 02:41:42','2015-05-08 15:07:55'),(11,8,19,0,0,0,0,'2015-05-06 02:41:42',NULL),(13,1,20,1,1,1,1,'2015-05-06 02:27:43','2015-05-07 17:39:45'),(14,2,20,1,1,1,1,'2015-05-06 02:27:43','2015-05-07 17:37:45'),(15,8,20,0,0,0,0,'2015-05-06 02:27:43',NULL),(17,1,21,1,1,1,1,'2015-05-06 02:47:43','2015-05-07 17:21:38'),(18,2,21,1,1,1,1,'2015-05-06 02:47:43','2015-05-07 17:51:58'),(19,8,21,0,0,0,0,'2015-05-06 02:47:43',NULL),(21,1,22,1,1,1,1,'2015-05-06 02:39:45','2015-05-07 17:04:46'),(22,2,22,1,1,1,1,'2015-05-06 02:39:45','2015-05-07 17:03:46'),(23,8,22,1,0,0,0,'2015-05-06 02:40:45','2015-05-08 03:10:03'),(25,1,23,1,1,1,1,'2015-05-06 02:17:46','2015-05-07 17:15:46'),(26,2,23,1,1,1,1,'2015-05-06 02:17:46','2015-05-07 17:14:46'),(27,8,23,1,1,1,1,'2015-05-06 02:17:46','2015-05-08 03:23:03');
+insert  into `role`(`kode_role`,`kode_group`,`kode_menu`,`view`,`itambah`,`iupdate`,`idelete`,`create_at`,`update_at`) values (1,1,17,1,1,1,1,'2015-05-06 02:37:41','2015-05-17 11:35:50'),(5,1,18,1,1,0,0,'2015-05-06 02:06:42','2015-05-11 15:25:35'),(9,1,19,1,1,1,1,'2015-05-06 02:41:42','2015-05-07 06:05:55'),(13,1,20,1,1,1,1,'2015-05-06 02:27:43','2015-05-07 17:39:45'),(17,1,21,1,1,1,1,'2015-05-06 02:47:43','2015-05-07 17:21:38'),(21,1,22,1,0,0,0,'2015-05-06 02:39:45','2015-05-11 15:50:34'),(25,1,23,1,1,0,0,'2015-05-06 02:17:46','2015-05-14 17:55:55'),(28,2,17,0,0,0,0,'2015-05-14 17:43:43',NULL),(29,2,18,0,0,0,0,'2015-05-14 17:43:43',NULL),(30,2,19,0,0,0,0,'2015-05-14 17:43:43',NULL),(31,2,20,0,0,0,0,'2015-05-14 17:43:43',NULL),(32,2,21,0,0,0,0,'2015-05-14 17:43:43',NULL),(33,2,22,0,0,0,0,'2015-05-14 17:43:43',NULL),(34,2,23,0,0,0,0,'2015-05-14 17:43:43',NULL);
 
 /*Table structure for table `user` */
 
@@ -91,11 +91,11 @@ CREATE TABLE `user` (
   PRIMARY KEY (`kode_user`),
   KEY `FK_user` (`kode_group`),
   CONSTRAINT `FK_user` FOREIGN KEY (`kode_group`) REFERENCES `group_user` (`kode_group`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
 
 /*Data for the table `user` */
 
-insert  into `user`(`kode_user`,`first_name`,`last_name`,`email`,`password`,`active`,`kode_group`,`create_at`,`update_at`) values (3,'Nur','Hidayatullah','kematjaya0@gmail.com','Az3HG264cGTGj1MlxzOHzPuYQzwjquekgIcpqJ07q4c',1,2,'2015-04-26 04:21:35','2015-05-04 03:37:08'),(6,'superuser','user','nur_hidayat_45@yahoo.com','Az3HG264cGTGj1MlxzOHzPuYQzwjquekgIcpqJ07q4c',1,1,'2015-04-26 05:17:29','2015-05-03 17:46:29'),(7,'ady','Setyawan','ady@gmail.com','Az3HG264cGTGj1MlxzOHzPuYQzwjquekgIcpqJ07q4c',1,8,'2015-05-07 06:19:48','2015-05-07 18:57:03');
+insert  into `user`(`kode_user`,`first_name`,`last_name`,`email`,`password`,`active`,`kode_group`,`create_at`,`update_at`) values (1,'superuser','user','nur_hidayat_45@yahoo.com','Pz4UWvfYSKak_LeURZoanO_VFT-QOG1MUbUEUwenl1c',1,1,'2015-04-26 05:17:29','2015-05-14 17:27:38'),(2,'Nur','Hidayatullah','kematjaya0@gmail.com','Az3HG264cGTGj1MlxzOHzPuYQzwjquekgIcpqJ07q4c',0,2,'2015-05-14 17:00:58',NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
