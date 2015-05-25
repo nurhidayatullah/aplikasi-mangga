d24919d3a3341fa89324bd32cd13f8681ba58306
nurhidayatullah <nur_hidayat_45@yahoo.com>
2015-05-25 15:13:41 +0700

    perbaiki footer

--- application/modules/admin/views/footer.php
+++ application/modules/admin/views/footer.php
@@ -48,7 +48,7 @@
 			$("#tb").dataTable({
 				"oLanguage": {
 						   "sLengthMenu": 'Display <select class="form-control">'+ 
-							 '<option value="1">1</option>'+
+							 '<option value="5">5</option>'+
 							'<option value="10">10</option>'+
 							'<option value="20">20</option>'+
 							'<option value="30">30</option>'+
